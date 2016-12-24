85524d4991ed78f31ae6d3059a647ba61ae1de9e
Cole R Lawrence <msgzht@gmail.com>
2016-12-24 15:34:44 -0600

    Remove app.scss and src/style

--- src/@app/app.component.ts
+++ src/@app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core'
 
-import '../style/app.scss';
-
 @Component({
   selector: 'pw-app', // <pw-app></pw-app>
   template: require('./app.component.html'),
-  styles: [ require('./app.component.scss') ],
+  styles: [
+    require('./app.component.scss'),
+  ],
 })
 export class AppComponent {
   constructor() {}
