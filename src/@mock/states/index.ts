5daca1c21bf3af8d9e9247c5b0be50ece9e5152a
Cole R Lawrence <msgzht@gmail.com>
2016-12-29 14:09:47 -0600

    Create Discover mock states

--- src/@mock/states/index.ts
+++ src/@mock/states/index.ts
@@ -1,5 +1,8 @@
 
 import { AppState, PartialAppState, DefaultAppState } from '@app/core'
+import moment = require('moment')
+
+import * as M from '@app/core/model'
 
 const mergeWith = <(obj: any, source: any, customizer?: (objValue, srcValue) => any) => any> require('lodash.mergewith')
 // https://lodash.com/docs/4.16.4#mergeWith
@@ -11,9 +14,128 @@ function mergeExceptArrays (objValue, srcValue) {
   }
 }
 
+
+// Profiles
+const pr$0: M.Profile = {
+  Id: 'pr0',
+  Tagline: 'Captain Action',
+  FirstName: 'Jackson',
+}
+// My Pod
+const pr$1: M.Profile = {
+  Id: 'pr$1',
+  Tagline: 'Meep! Meep!',
+  FirstName: 'Road',
+}
+const pr$2: M.Profile = {
+  Id: 'pr$2',
+  Tagline: 'I tawt I taw a puddy-tat!',
+  FirstName: 'Tweety',
+}
+
+const pra1: M.Profile = {
+  Id: 'pra1',
+  Tagline: 'What a maroon!',
+  FirstName: 'Bugs',
+}
+const pra2: M.Profile = {
+  Id: 'pra2',
+  Tagline: 'You\'re dethpicable',
+  FirstName: 'Daffy',
+}
+const pra3: M.Profile = {
+  Id: 'pra4',
+  Tagline: 'Y-y-you can\'t fool me.',
+  FirstName: 'Porky',
+}
+const pra4: M.Profile = {
+  Id: 'pra3',
+  Tagline: 'Don\'t ever call me doll',
+  FirstName: 'Lola',
+}
+const prb1: M.Profile = {
+  Id: 'prb1',
+  Tagline: 'Great horny toads!',
+  FirstName: 'Yosemite',
+}
+const prb2: M.Profile = {
+  Id: 'prb2',
+  Tagline: 'Gweat gwasshoppers!',
+  FirstName: 'Elmer',
+}
+const prc1: M.Profile = {
+  Id: 'prc1',
+  Tagline: 'Arriba! Arriba!',
+  FirstName: 'Speedy',
+}
+const prc2: M.Profile = {
+  Id: 'prc2',
+  Tagline: 'YEeooooowwwwwwwww!!!',
+  FirstName: 'Wile',
+}
+
+// Friends
+const fr1: M.Friend = nfri(pr$1, M.FriendStatus.IN_GROUP)
+const fr2: M.Friend = nfri(pr$2, M.FriendStatus.ONLINE)
+
+// Peas and Pods
+
+// Pod $: Jackson (me), Road
+const pe$0: M.Pea = npea(pr$0, { start: 1, end: 3.5 })
+const pe$1: M.Pea = npea(pr$1, { start: .5, end: 3 })
+const pe$2: M.Pea = npea(pr$2, { start: 1, end: 4 })
+const pod$: M.Pod = npod('$', pe$0, pe$1, pe$2)
+
+// Pod A: Bugs, Daffy, Lola, Porky
+const pea1: M.Pea = npea(pra1, { start: 1, end: 3 })
+const pea2: M.Pea = npea(pra2, { start: 1.5, end: 3 })
+const pea3: M.Pea = npea(pra3, { start: 1, end: 2.5 })
+const pea4: M.Pea = npea(pra4, { start: 1, end: 3 })
+const poda: M.Pod = npod('a', pea1, pea2, pea3, pea4)
+
+// Pod B: Yosemite, Elmer
+const peb1: M.Pea = npea(prb1, { start: 1, end: 3 })
+const peb2: M.Pea = npea(prb2, { start: 1.5, end: 3 })
+const podb: M.Pod = npod('b', peb1, peb2)
+
+// Pod C: Speedy, Wile
+const pec1: M.Pea = npea(prc1, { start: 4, end: 7 })
+const pec2: M.Pea = npea(prc2, { start: 4.5, end: 7 })
+const podc: M.Pod = npod('c', pec1, pec2)
+
+function nfri(profile: M.Profile, status: M.FriendStatus): M.Friend {
+  return {
+    Id: profile.Id + '+fri',
+    Profile: profile,
+    Status: status
+  }
+}
+// Create new Pea Model using provided profile and availability
+// Availability is represented as hours relative from time now.
+function npea(profile: M.Profile, avail_rel_hours: { start: number, end: number }): M.Pea {
+  const {start, end} = avail_rel_hours
+  return {
+    Id: profile.Id + '+pea',
+    Profile: profile,
+    Availability: [{ Id: profile.Id + '+av', Start: reltime(start, 'h'), End: reltime(end, 'h') }],
+  }
+}
+function npod(id: string, ...peas: M.Pea[]): M.Pod {
+  return {
+    Id: id + '+pod',
+    Peas: peas,
+  }
+}
+
+// Relative time from now
+function reltime(rel: number, unit: moment.unitOfTime.DurationConstructor): Date {
+  return moment().add(rel, unit).toDate()
+}
+
 export
 type MockState = { name: string, state: AppState }
 
+// States
 const MOCK_STATES: MockState[] = (function () {
   let res: MockState[] = []
   let add = state(res)
@@ -58,11 +180,7 @@ const MOCK_STATES: MockState[] = (function () {
       Credentials: {
         FirstName: 'Jackson',
         LastName: 'Rains',
-        Profile: {
-          Id: 'jr0p',
-          Tagline: 'Captain Action',
-          FirstName: 'Jackson',
-        },
+        Profile: pr$0,
         Id: 'jr0'
       },
       IsLoggedIn: true,
@@ -79,14 +197,10 @@ const MOCK_STATES: MockState[] = (function () {
   = assign(notLoggedInBase, {
     Login: {
       Credentials: {
-        FirstName: 'Cole',
-        LastName: 'Lawrence',
-        Profile: {
-          Id: 'cl0p',
-          Tagline: 'Mostly Curious',
-          FirstName: 'Cole',
-        },
-        Id: 'cl0'
+        FirstName: 'Bugs',
+        LastName: 'Bunny',
+        Profile: pra1,
+        Id: 'pa0'
       },
       IsLoggedIn: true,
       UserItems: [],
@@ -96,6 +210,50 @@ const MOCK_STATES: MockState[] = (function () {
   add(//////////////////
     'Login/Logged in 2',
     loggedInBase2
+  )
+
+  const discoverBase1: AppState
+  = assign(loggedInBase1, {
+    Device: {
+      URL: '/discovering'
+    },
+    Discover: {
+      Pea: pe$0,
+      Pod: pod$,
+      Matches: <M.PodMatch[]> [
+        { Id: 'm-a', Pod: poda, Status: M.PodMatchStatus.NOT_RESPONDED },
+        { Id: 'm-b', Pod: podb, Status: M.PodMatchStatus.ACCEPTED },
+        { Id: 'm-c', Pod: podc, Status: M.PodMatchStatus.REJECTED },
+        { Id: 'm-a1', Pod: poda, Status: M.PodMatchStatus.NOT_RESPONDED },
+        { Id: 'm-b1', Pod: podb, Status: M.PodMatchStatus.NOT_RESPONDED },
+        { Id: 'm-c1', Pod: podc, Status: M.PodMatchStatus.NOT_RESPONDED },
+      ],
+      IsDiscovering: true,
+      ShowDiscover: true,
+      IsPodLocked: false,
+      InvitationOptions: {
+        Friends: [ fr1, fr2 ]
+      },
+      ShowAvailabilityOptions: false,
+      ShowInvitationOptions: false
+    }
+  })
+
+  add(//////////////////
+    'Discover/Base',
+    discoverBase1
+  )
+
+  add(//////////////////
+    'Discover/Show Availability',
+    discoverBase1,
+    { Discover: { ShowAvailabilityOptions: true } }
+  )
+
+  add(//////////////////
+    'Discover/Show Invitations',
+    discoverBase1,
+    { Discover: { ShowInvitationOptions: true } }
   )
 
   // TODO Future mocks please retain comment delimiters,
