92e59a166aa997135a55e59d6eb24df446c265c0
Cole R Lawrence <msgzht@gmail.com>
2017-01-09 04:58:35 -0600

    Lovely shader detached from consumer

--- src/@app/shared/shadow-scroller.directive.ts
+++ src/@app/shared/shadow-scroller.directive.ts
@@ -6,12 +6,13 @@ import { Directive, HostListener, ElementRef, OnDestroy } from '@angular/core'
   selector: '[pw-shadow-scroller]'
 })
 export class ShadowScrollerDirective implements OnDestroy {
+  _shader: HTMLDivElement
   @HostListener('scroll') onScroll() {
-    autoShade(this._ref.nativeElement)
+    this.fn()
   }
-  fn = () => autoShade(this._ref.nativeElement)
+  fn = () => autoShade(this._ref.nativeElement, this._shader)
   constructor(private _ref: ElementRef) {
-    addStyles(this._ref.nativeElement)
+    this._shader = addStyles(this._ref.nativeElement)
     window.addEventListener("resize", this.fn)
     setTimeout(this.fn, 0)
   }
@@ -21,10 +22,22 @@ export class ShadowScrollerDirective implements OnDestroy {
   }
 }
 
-function addStyles(elt: HTMLElement) {
+function addStyles(elt: HTMLElement): HTMLDivElement {
   elt.style.overflowY = 'auto'
-  elt.style.transition = 'box-shadow .4s ease-in'
   elt.style.flex = "1"
+  const shader = document.createElement('div')
+  Object.assign(shader.style, {
+    position: 'absolute',
+    zIndex: '10',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    pointerEvents: 'none',
+    transition: 'box-shadow .4s ease-in'
+  })
+  elt.appendChild(shader)
+  return shader
 }
 
 const topShadow = `inset 0 13px 10px -10px rgba(0,0,0,0.2)`
@@ -32,7 +45,7 @@ const topShadow0 = `inset 0 13px 10px -10px rgba(0,0,0,0)`
 const btmShadow = `inset 0 -13px 10px -10px rgba(0,0,0,0.2)`
 const btmShadow0 = `inset 0 -13px 10px -10px rgba(0,0,0,0)`
 
-function autoShade(elt: HTMLElement) {
+function autoShade(elt: HTMLElement, shader: HTMLElement) {
   let height = elt.offsetHeight
   let scrollHeight = elt.scrollHeight
   let scrollTop = elt.scrollTop
@@ -49,9 +62,5 @@ function autoShade(elt: HTMLElement) {
     shadows.push(btmShadow0)
   }
 
-  if (shadows.length) {
-    elt.style.boxShadow = shadows.join(', ')
-  } else {
-    elt.style.boxShadow = null
-  }
+  shader.style.boxShadow = shadows.join(', ')
 }
